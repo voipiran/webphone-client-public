e8808ee3c95bb3bd641f0e72d8e9ab3221560291
Mehdi <mahdi064@gmail.com>
2021-02-04 20:32:40 +0330

    webphone routes created

--- app/Http/Controllers/WebphoneController.php
+++ app/Http/Controllers/WebphoneController.php
@@ -13,7 +13,7 @@ class WebphoneController extends Controller
      */
     public function index()
     {
-        //
+        return view('dashboard.webphones.browse');
     }
 
     /**
--- routes/dashboard.php
+++ routes/dashboard.php
@@ -1,8 +1,11 @@
 <?php
-use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\WebphoneController;
 
 Route::group(['prefix' => 'dashboard' , 'middleware' => ['auth']], function () {
     Route::get('/' , [HomeController::class , 'index'] )->name('dashboard');    
-    Route::resource('webphones', 'WephoneController');
+    Route::resources([
+        'webphones' => WebphoneController::class 
+    ]);
 });
